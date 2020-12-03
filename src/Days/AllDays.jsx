2d7a6de3ec1311aa8e347c0796d54e9f12ed611b
JNieuwhuis <J.Nieuwhuis@hotmail.com>
2020-12-03 11:41:19 +0100

    alldays

---
+++ src/Days/AllDays.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Day1 from './Day1/Day1';
+import Day2 from './Day2/Day2';
+import Day3 from './Day3/Day3';
+
+const AllDays = () => {
+    return (
+        <>
+            <Day1 />
+            <Day2 />
+            <Day3 />
+        </>
+    );
+}
+
+export default AllDays;
